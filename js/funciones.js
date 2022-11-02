691259bfae776d7537f9d06adb584ea8fd5f0372
L1L3V4 <eva.s284913@cesurformacion.com>
2022-11-02 23:22:47 +0100

    final

--- js/funciones.js
+++ js/funciones.js
@@ -4,7 +4,9 @@ var sesion = sesion = document.getElementById("formulario");
 var URLactual = new URLSearchParams(window.location.search);
 
 var usuario = URLactual.get("Usuario");
+if(usuario != null){
+    document.getElementById('uwu').textContent= "Bienvenid@ " + usuario + " !";
+}
 
-document.getElementById('uwu').textContent=usuario ;
 
 
